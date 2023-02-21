9d1d5cd1963f33d90de47dc814f2254cf82cec84
bugcat9 <1767508581@qq.com>
2023-02-21 22:33:54 +0800

    fix:修改difference

--- Difference.h
+++ Difference.h
@@ -2,47 +2,48 @@
 #define _DIFFERENCE_H
 
 #include <vector>
-namespace tiny
+
+/**
+ * @brief 差分数组，用来求区间加减
+ * 
+ */
+class Difference
 {
-    class Difference
+private:
+    std::vector<int> diff;
+
+public:
+    Difference(std::vector<int> nums)
     {
-    private:
-        std::vector<int> diff;
-
-    public:
-        Difference(std::vector<int> nums)
         {
+            diff.resize(nums.size());
+            diff[0] = nums[0];
+            for (int i = 1; i < nums.size(); i++)
             {
-                diff.resize(nums.size());
-                diff[0] = nums[0];
-                for (int i = 1; i < nums.size(); i++)
-                {
-                    diff[i] = nums[i] - nums[i - 1];
-                }
+                diff[i] = nums[i] - nums[i - 1];
             }
         }
+    }
 
-        void increment(int i, int j, int val)
+    void increment(int i, int j, int val)
+    {
+        diff[i] += val;
+        if (j + 1 < diff.size())
         {
-            diff[i] += val;
-            if (j + 1 < diff.size())
-            {
-                diff[j + 1] -= val;
-            }
+            diff[j + 1] -= val;
         }
+    }
 
-        std::vector<int> result()
+    std::vector<int> result()
+    {
+        std::vector<int> res(diff.size());
+        res[0] = diff[0];
+        for (int i = 1; i < diff.size(); i++)
         {
-            std::vector<int> res(diff.size());
-            res[0] = diff[0];
-            for (int i = 1; i < diff.size(); i++)
-            {
-                res[i] = res[i - 1] + diff[i];
-            }
-            return res;
+            res[i] = res[i - 1] + diff[i];
         }
-    };
-
-} // namespace tiny
+        return res;
+    }
+};
 
 #endif // _DIFFERENCE_H
--- test/Difference_test.cpp
+++ test/Difference_test.cpp
@@ -5,7 +5,7 @@
 BOOST_AUTO_TEST_CASE(case0)
 {
     std::vector<int> nums{1, 2, 3, 4, 5, 6, 7, 8};
-    tiny::Difference df(nums);
+    Difference df(nums);
     df.increment(1, 3, 1);
     BOOST_CHECK_EQUAL(df.result()[1], 3);
     BOOST_CHECK_EQUAL(df.result()[2], 4);
